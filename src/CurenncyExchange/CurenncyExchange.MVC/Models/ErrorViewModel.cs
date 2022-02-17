0e59c5d0c2e9f7fbaf4a1f512daf9e5454437d61
Guliev Vakhid <01.unbroken@gmail.com>
2022-02-17 11:34:32 +0300

    Add a new mvc aplication

---
+++ src/CurenncyExchange/CurenncyExchange.MVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CurenncyExchange.MVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
