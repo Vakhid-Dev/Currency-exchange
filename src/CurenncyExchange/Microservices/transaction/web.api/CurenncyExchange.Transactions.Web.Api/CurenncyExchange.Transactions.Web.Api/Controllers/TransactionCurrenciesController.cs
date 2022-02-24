3efdced9f136fdf0a43fbb59ce730e2d55c802d0
Guliev Vakhid <01.unbroken@gmail.com>
2022-02-24 08:03:08 +0300

    Added controllers for transaction controller

--- src/CurenncyExchange/Microservices/transaction/web.api/CurenncyExchange.Transactions.Web.Api/CurenncyExchange.Transactions.Web.Api/Controllers/TransactionCurrenciesController.cs
+++ src/CurenncyExchange/Microservices/transaction/web.api/CurenncyExchange.Transactions.Web.Api/CurenncyExchange.Transactions.Web.Api/Controllers/TransactionCurrenciesController.cs
@@ -1,13 +1,7 @@
 ﻿#nullable disable
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
-using CurenncyExchange.Data.Context;
 using CurenncyExchange.Transaction.Core;
+using CurenncyExchange.App.Service;
 
 namespace CurenncyExchange.Transactions.Web.Api.Controllers
 {
@@ -15,94 +9,36 @@ namespace CurenncyExchange.Transactions.Web.Api.Controllers
     [ApiController]
     public class TransactionCurrenciesController : ControllerBase
     {
-        private readonly TransactionContext _context;
+        private readonly ITransactionService _transactionService;
 
-        public TransactionCurrenciesController(TransactionContext context)
+        public TransactionCurrenciesController(ITransactionService transactionService)
         {
-            _context = context;
+            _transactionService = transactionService;
         }
 
         // GET: api/TransactionCurrencies
         [HttpGet]
         public async Task<ActionResult<IEnumerable<TransactionCurrency>>> GetTransactionDetails()
         {
-            return await _context.TransactionDetails.ToListAsync();
+            return null;
         }
 
         // GET: api/TransactionCurrencies/5
         [HttpGet("{id}")]
         public async Task<ActionResult<TransactionCurrency>> GetTransactionCurrency(Guid id)
         {
-            var transactionCurrency = await _context.TransactionDetails.FindAsync(id);
-
-            if (transactionCurrency == null)
-            {
-                return NotFound();
-            }
-
-            return transactionCurrency;
-        }
-
-        // PUT: api/TransactionCurrencies/5
-        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
-        [HttpPut("{id}")]
-        public async Task<IActionResult> PutTransactionCurrency(Guid id, TransactionCurrency transactionCurrency)
-        {
-            if (id != transactionCurrency.Id)
-            {
-                return BadRequest();
-            }
-
-            _context.Entry(transactionCurrency).State = EntityState.Modified;
-
-            try
-            {
-                await _context.SaveChangesAsync();
-            }
-            catch (DbUpdateConcurrencyException)
-            {
-                if (!TransactionCurrencyExists(id))
-                {
-                    return NotFound();
-                }
-                else
-                {
-                    throw;
-                }
-            }
-
-            return NoContent();
+            return null ;
         }
 
         
         [HttpPost]
         public async Task<ActionResult<TransactionCurrency>> PostTransactionCurrency(TransactionCurrency transactionCurrency)
         {
-            _context.TransactionDetails.Add(transactionCurrency);
-            await _context.SaveChangesAsync();
+          await _transactionService.ExecuteAsync(transactionCurrency);
 
             return CreatedAtAction("GetTransactionCurrency", new { id = transactionCurrency.Id }, transactionCurrency);
         }
 
-        // DELETE: api/TransactionCurrencies/5
-        [HttpDelete("{id}")]
-        public async Task<IActionResult> DeleteTransactionCurrency(Guid id)
-        {
-            var transactionCurrency = await _context.TransactionDetails.FindAsync(id);
-            if (transactionCurrency == null)
-            {
-                return NotFound();
-            }
 
-            _context.TransactionDetails.Remove(transactionCurrency);
-            await _context.SaveChangesAsync();
-
-            return NoContent();
-        }
-
-        private bool TransactionCurrencyExists(Guid id)
-        {
-            return _context.TransactionDetails.Any(e => e.Id == id);
-        }
     }
 }
