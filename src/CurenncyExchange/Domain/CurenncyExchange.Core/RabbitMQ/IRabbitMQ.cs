1b7686faa1f4b341835bbeb3e490932068b6580b
Guliev Vakhid <01.unbroken@gmail.com>
2022-03-01 11:17:16 +0300

    Removed unused comment

--- src/CurenncyExchange/Domain/CurenncyExchange.Core/RabbitMQ/IRabbitMQ.cs
+++ src/CurenncyExchange/Domain/CurenncyExchange.Core/RabbitMQ/IRabbitMQ.cs
@@ -2,7 +2,6 @@
 {
     public interface IRabbitMQ
     {
-        // public void SendMessage(object obj);
         public Task SendMessage(object message);
         public Task SendMessage(string message);
         
